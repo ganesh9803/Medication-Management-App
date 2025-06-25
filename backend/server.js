98e2437d49e1a14007d6a51f62bc771a16b91a5b
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-06-25 23:12:47 +0530

    m-6

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
+import path from 'path';
 import express from 'express';
-import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import path from 'path';
-
-import router from '../src/routes/index.js';
-import '../src/jobs/updateMissedAdherence.js';
 
 dotenv.config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads'))); // serve static files
+
+import router from './routes/index.js';
+import './jobs/updateMissedAdherence.js';
 
 app.use('/api', router);
+
+const PORT = process.env.PORT || 5000;
 
 // Error handling middleware
 app.get('/',(req,res)=> {
   res.send("API Working")
 })
 
-app.listen(port, ()=>console.log('Sever started on PORT : ' + port))
+app.listen(PORT , ()=>console.log('Sever started on PORT : ' + PORT ))
+
+export default app;
