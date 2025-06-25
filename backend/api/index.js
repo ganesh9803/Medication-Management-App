94cab9218d2b682979e6658b5a5ccfc1be9eb17a
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-06-25 22:51:56 +0530

    m-2

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
 
+import router from '../src/routes/index.js';
+import '../src/jobs/updateMissedAdherence.js';
+
 dotenv.config();
 
 const app = express();
@@ -11,10 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-import router from '../src/routes/index.js';
-import '../src/jobs/updateMissedAdherence.js';
-
 app.use('/api', router);
-app.get('/', (req, res) => res.send('API Working from Vercel'));
+
+app.get('/', (req, res) => {
+  res.send('✅ API Working from Vercel');
+});
 
 export const handler = serverless(app);
