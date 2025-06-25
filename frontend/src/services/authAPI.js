543e6de3727cc41a5a671eafe849c2014ae5a557
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-06-26 00:47:49 +0530

    h1

--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { getToken } from '../Utility/auth';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use(config => {
